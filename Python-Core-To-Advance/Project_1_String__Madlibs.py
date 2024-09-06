515e5918ddf6de935c6452eac20454d97b936403
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-09-06 18:31:26 +0100

    Create Project_1_String__Madlibs.py

---
+++ Python-Core-To-Advance/Project_1_String__Madlibs.py
@@ -0,0 +1,8 @@
+A = input("Adjective: ")
+V1 = input("Verb-1: ")
+V2 = input("Verb-2: ")
+P = input("Famous Person Name: ")
+
+ans = f"Computer programming is so {A}! It makes me so excited all the time because I love to {V1}. Stay hydrated and {V2} like you are {P}!"
+
+print(ans)
